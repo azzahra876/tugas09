956261b6419505c4f52d1d4bb190dd3efbe4f146
azza <moh119281@gmail.com>
2025-10-26 17:40:22 +0700

    comitan2

--- koneksi.php
+++ koneksi.php
@@ -3,8 +3,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 $host = "localhost";   
-$user = "root";      
-$pass = "";     
-$db   = "db_xirpl1_9_1"; 
+$user = "xirpl1-9";      
+$pass = "0097119849";     
+$db   = "db_xirpl1-9_1"; 
 
 $koneksi = mysqli_connect($host, $user, $pass, $db);
